e4d6b4948f4d1b88a0f92e5e50205e8217d8968b
Justin H <justinghughes134@gmail.com>
2022-10-18 10:19:41 -0400

    Create color selector

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const gridContainer = document.getElementById('grid-container');
-        const button = document.querySelector('button');
-        button.addEventListener('click', () => {
+const button = document.querySelector('button');
+const colorSelector = document.getElementById('colors');
+button.addEventListener('click', () => {
         createGrid();
     });
         const resetBtn = document.querySelector('#reset');
@@ -11,6 +12,9 @@ const gridContainer = document.getElementById('grid-container');
         function clearGrid() {
             gridContainer.innerHTML = '';
         }
+
+        const redBtn = document.querySelector('#red');
+        const blueBtn = document.querySelector('#blue');
 
     function createGrid() {
         const userSelection = prompt("Choose a grid size from 1-100");
@@ -23,9 +27,37 @@ const gridContainer = document.getElementById('grid-container');
                 let newGrid = document.createElement('div');
                 newGrid.classList.add('grid-item');
                 gridContainer.appendChild(newGrid);
+
+                colorSelector.addEventListener('change', function() {
+                    if (this.value == "red") {
+                        redGrid();
+                    } else {
+                        blueGrid();
+                    }
+                });
+
+                function redGrid() {
+                newGrid.addEventListener('mouseover', () => {
+                newGrid.style.backgroundColor = 'red';
+                })};
+            
+                function blueGrid() {
                 newGrid.addEventListener('mouseover', () => {
                 newGrid.style.backgroundColor = 'blue';
-        })};
-    };
-        gridContainer.style.gridTemplateColumns = `repeat(${userSelection}, ${squareSize}`;
-};
+                })}}};
+                    
+            gridContainer.style.gridTemplateColumns = `repeat(${userSelection}, ${squareSize}`;
+            };
+
+
+
+
+
+
+// redSelection.addEventListener('click', () => {
+//     console.log("Red")
+// });
+
+// blueSelection.addEventListener('click', () => {
+//     console.log("Blue")
+// });
