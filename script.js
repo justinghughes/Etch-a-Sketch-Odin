fe7fd72a07f143e6d92e3fde0b4089dde2f9e61d
Justin H <justinghughes134@gmail.com>
2022-10-18 10:42:54 -0400

    Add default 16x16 grid

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const gridContainer = document.getElementById('grid-container');
 const button = document.querySelector('button');
 const colorSelector = document.getElementById('colors');
 button.addEventListener('click', () => {
+        gridContainer.innerHTML = '';
         createGrid();
     });
         const resetBtn = document.querySelector('#reset');
@@ -16,6 +17,11 @@ button.addEventListener('click', () => {
         const redBtn = document.querySelector('#red');
         const blueBtn = document.querySelector('#blue');
 
+        for (let i = 0; i < 256; i++) {
+        let newGrid = document.createElement('div');
+                newGrid.classList.add('grid-item');
+                gridContainer.appendChild(newGrid);
+        }
     function createGrid() {
         const userSelection = prompt("Choose a grid size from 1-100");
         let squareSize = (parseInt("800px".replace(/px/,""))/userSelection)+"px";
